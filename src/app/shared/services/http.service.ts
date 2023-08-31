6730c11cbb3bd0486c55fc6bb0e618f80b7672ea
GabrielCampos07 <siginmegain@hotmail.com>
2023-08-31 20:47:18 -0300

    Só removendo o desnecessário

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -4,7 +4,7 @@ import { environment } from './../../../environments/environment';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import {
   DomSanitizer,
   SafeResourceUrl,
