56100242c390e2c051237316afbc215e43e3865c
GabrielCampos07 <siginmegain@hotmail.com>
2023-09-02 00:54:58 -0300

    Arrumando botão desnecessário

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material.module';
 import { TableModule } from './components/table/table.module';
 import { FilterButtonComponent } from './components/filter-button/filter-button.component';
-import { SearchButtonComponent } from './components/search-button/search-button.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from './components/form/form.component';
 import { CommonModule } from '@angular/common';
@@ -10,17 +9,11 @@ import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   imports: [MaterialModule, CommonModule, ReactiveFormsModule, FormsModule],
-  declarations: [
-    FilterButtonComponent,
-    SearchButtonComponent,
-    FormComponent,
-    InputComponent,
-  ],
+  declarations: [FilterButtonComponent, FormComponent, InputComponent],
   exports: [
     MaterialModule,
     TableModule,
     FilterButtonComponent,
-    SearchButtonComponent,
     FormComponent,
     CommonModule,
     InputComponent,
